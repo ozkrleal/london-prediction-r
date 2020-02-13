e9f77715f40d680452555399a9c92833cab7e388
Oscar Leal - alienware <ozkralm10@hotmail.com>
2020-02-13 14:05:55 +0100

    idea: ln to prices, ratingreviewcount, rating ta count, AND sqr to distance-alter and distance

--- DA3-labs/homework3/hoteur_data_prep.R
+++ DA3-labs/homework3/hoteur_data_prep.R
@@ -19,9 +19,9 @@ data <- merge(price, features)
 
 data <- data %>% filter(city %in% c('Berlin', 'Munich', 'Vienna', 'Budapest', 'Prague', 'Warsaw'))
 
-data <- data %>% filter(year == 2017 & month == 11 & weekend == 0)
+data <- data %>% filter(year == 2017 & month == 11 & weekend == 0 & accommodation_type == "Hotel")
 
-data <- data %>% select(- c(year, month, weekend))
+data <- data %>% select(- c(year, month, weekend, accommodation_type))
 
 
 
@@ -29,5 +29,43 @@ glimpse(data)
 skimr::skim(data)
 summary(data)
 
-DataExplorer::create_report(data)
+#DataExplorer::create_report(data)
+
+DataExplorer::introduce(data)
+DataExplorer::plot_intro(data)
+DataExplorer::plot_missing(data)
+
+## Left: frequency distribution of all discrete variables
+DataExplorer::plot_bar(data)
+## Right: `price` distribution of all discrete variables
+DataExplorer::plot_bar(data, with = "price")
+
+DataExplorer::plot_histogram(data)
+DataExplorer::plot_density(data)
+
+DataExplorer::plot_qq(data, by = "stars")
+
+DataExplorer::plot_correlation(data)
+
+DataExplorer::plot_scatterplot(split_columns(data)$continuous, by = "price", sampled_rows = 1000L)
+
+
+
+#### Data Gen and Descriptives
+
+
+data <- DataExplorer::dummify(data, select = 'offer_cat')
+
+data <- data %>%
+  mutate(agesq = age^2,
+         agecu = age^3)
+
+##### Regression Analysis
+
+ggplot(data = data, aes(x=stars, y=price)) +
+  geom_point(size=2,  shape=20, stroke=2, fill=color[3], color=color[3]) +
+  geom_smooth(method="loess", se=F, colour=color[1], size=1.5, span=0.9) +
+  labs(x = "Stars",y = "Price") +
+  theme_bg() +
+  scale_x_continuous(limits = c(0,5), breaks = seq(0,30, 10))
 
